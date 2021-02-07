eb56c2d98cc1186741b97ad7ea5ea68c9c360c7b
David Beaumont <david.beaumont@birdie.care>
2021-02-07 15:53:31 +0000

    Show a success page when the code is confirmed

--- db.js
+++ db.js
@@ -1,1 +1,29 @@
-// TODO
+const sqlite3 = require('sqlite3');
+const { open } =require('sqlite');
+
+function getDb() {
+  return open({
+    filename: './db.sqlite',
+    driver: sqlite3.Database,
+  });
+}
+
+async function checkAndMarkCompleted(code) {
+  const db = await getDb();
+
+  const sql = `
+    UPDATE vaccination_code
+    SET confirmed = true
+    WHERE code = $code
+    AND confirmed = false;
+  `;
+
+  const result = await db.run(sql, { $code: code });
+
+  if (result.changes !== 1) {
+    console.error(result);
+    throw new Error('Code invalid or already confirmed');
+  }
+}
+
+module.exports = { checkAndMarkCompleted };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const sassMiddleware = require('node-sass-middleware');
 const path = require('path');
+const db = require('./db');
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -32,10 +33,15 @@ app.get('/enter-code', (req, res) => {
   res.render('enter-code');
 });
 
-app.post('/enter-code', (req, res) => {
-  // TODO - query database and update in a transaction
-  // TODO - render error page if there was a problem
-  res.render('success');
+app.post('/enter-code', async (req, res) => {
+  const { code } = req.body;
+
+  try {
+    await db.checkAndMarkCompleted(code);
+    res.render('success');
+  } catch (err) {
+    res.render('enter-code');
+  }
 });
 
 app.listen(port, () => {
